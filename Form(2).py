e2898ecf3b3d74f72503b63921253a474413509e
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-07-07 19:02:53 +0800

    Update Form(2).py

--- Form(2).py
+++ Form(2).py
@@ -4,4 +4,5 @@ import webbrowser
 
 st.markdown("<h1 style='text-align: center; color: black;'>Terima Kasih</h1>", unsafe_allow_html=True)
 st.write("Terima Kasih atas laporannya, untuk melaporkan pelaporan lainnya silahkan klik link berikut")
-st.markdown('Please <a href="https://laporst2023-tabanan.streamlit.app/" target="_blank">Klik link berikut</a>, unsafe_allow_html=True')
+st.markdown('[Klik link berikut](https://laporst2023-tabanan.streamlit.app/)', unsafe_allow_html=True)
+#st.markdown('Please <a href="https://laporst2023-tabanan.streamlit.app/" target="_blank">Klik link berikut</a>, unsafe_allow_html=True')

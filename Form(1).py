09d50ca4ac4abdd351d8ab4a67382ca5c76c0a8b
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-07-07 23:15:46 +0800

    Update Form(1).py

--- Form(1).py
+++ Form(1).py
@@ -1,7 +1,7 @@
-from logging import PlaceHolder
 import streamlit as st
 import pandas as pd
 from datetime import date
+import time
 from streamlit_js_eval import streamlit_js_eval
 
 sheet_url = "https://docs.google.com/spreadsheets/d/13BbpP9ox-XCo3xB74eTTG0oFoI_aIt6w_BP-4hU3Sjg/edit#gid=0"
@@ -59,6 +59,7 @@ if __name__ == "__main__":
 
                 if (len(JumlahRuta) != 0 and len(JumlahL2) != 0 and len(JumlahL2kePML) != 0 and len(JumlahL2keKoseka) != 0 and SudahSelesai != "PILIH" and SudahIsiRepo != "PILIH"):
                     if st.button('Submit', "https://laporst2023-tabanan.streamlit.app/"):
-                        st.success(f'Kecamatan : {FirstFilter}, Desa : {len(JumlahRuta)}', icon="✅")
+                        st.success(f'Data berhasil tersubmit', icon="✅")
+                        time.sleep(1.5)
                         streamlit_js_eval(js_expressions="parent.window.location.reload()")
                         #st.markdown(f'window.open("{"https://laporst2023-tabanan.streamlit.app/"}", "_blank");')

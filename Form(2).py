2ebf13dbf1ef3096c119040127c9f4b530c30957
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-07-07 16:31:28 +0800

    Link Terima Kasih

---
+++ Form(2).py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import webbrowser
+
+st.markdown("<h1 style='text-align: center; color: black;'>Terima Kasih</h1>", unsafe_allow_html=True)
+st.write("Terima Kasih atas laporannya, untuk melaporkan pelaporan lainnya silahkan klik link berikut")
+st.write("[klik link berikut](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
